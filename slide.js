19c22fb5926687cd4f7d7cbe73e5bd7bf1ffde86
MarkKapileo <mark.kapileo@my.marianas.edu>
2023-12-01 15:20:23 +1000

    added gallery transitions

--- slide.js
+++ slide.js
@@ -1,17 +1,24 @@
-const slider = document.createElement('input');
-slider.type = 'range';
-slider.min = '1';
-slider.max = document.querySelectorAll('#image-slider img').length.toString();
-slider.value = '1';
-slider.addEventListener('input', updateSlider);
+let currentIndex = 0;
 
-document.body.insertBefore(slider, document.getElementById('image-slider'));
-
-function updateSlider() {
-  const index = parseInt(slider.value) - 1;
-  const images = document.querySelectorAll('#image-slider img');
-  
-  images.forEach((img, i) => {
-    img.style.display = i === index ? 'block' : 'none';
-  });
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const slideWidth = document.querySelector('.slide').offsetWidth;
+    slides.style.transform = `translateX(${-index * slideWidth}px)`;
 }
+
+function nextSlide() {
+    const totalSlides = document.querySelectorAll('.slide').length;
+    currentIndex = (currentIndex + 2) % totalSlides; // Show 2 images at a time
+    showSlide(currentIndex);
+}
+if (currentIndex === 0) {
+    setTimeout(() => showSlide(currentIndex), 500);
+}
+function prevSlide() {
+    const totalSlides = document.querySelectorAll('.slide').length;
+    currentIndex = (currentIndex - 2 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+}
+
+// Auto slide every 5 seconds
+setInterval(nextSlide, 5000);
